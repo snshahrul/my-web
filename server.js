5c45d16f64988c4b2f88b368bfd51b526a6518c3
Shahrul Azmi Salim Shah <shahrulazmisalimshah@gmail.com>
2026-05-07 22:25:36 +0800

    Initialize Express server with static file serving

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the root directory
+app.use(express.static(__dirname));
+
+// For any request that doesn't match a static file, serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
